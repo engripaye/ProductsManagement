41b5c69517e4dfc313c88082a92cce1f9541d8d1
mac <@b.tunde.ipaye@gmail.com>
2026-09-14 02:54:15 +0100

    feat: http swagger ui impl commit by ENGR. IPAYE

--- ProductsManagement/Program.cs
+++ ProductsManagement/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
