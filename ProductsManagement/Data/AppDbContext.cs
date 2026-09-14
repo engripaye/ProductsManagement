167a023da70c5ba3e17f2c22c5488e25914deeff
mac <@b.tunde.ipaye@gmail.com>
2026-09-14 20:11:00 +0100

    feat: database context impl commit by ENGR. IPAYE

---
+++ ProductsManagement/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProductsManagement.Models;
+
+namespace ProductsManagement.Data;
+
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    
+    public DbSet<Product> Products => Set<Models.Product>();
+    
+}   
