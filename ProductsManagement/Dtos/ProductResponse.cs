d571c5d18d5a0b9adf4d465b71e44f3969fc9054
mac <@b.tunde.ipaye@gmail.com>
2026-09-16 20:07:57 +0100

    feat: product response impl. commit by ENGR. IPAYE

---
+++ ProductsManagement/Dtos/ProductResponse.cs
@@ -0,0 +1,8 @@
+namespace ProductsManagement.Dtos;
+
+public class ProductResponse
+{
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+}
