1fcde59b4b9664846d2cde7f5901e2d699d2772b
mac <@b.tunde.ipaye@gmail.com>
2026-09-16 19:05:54 +0100

    refractor: product service updated commit by ENGR. IPAYE

--- ProductsManagement/Services/IProductService.cs
+++ ProductsManagement/Services/IProductService.cs
@@ -7,7 +7,7 @@ public interface IProductService
     IEnumerable<Product> GetAllProducts();
     Product? GetProductById(int id);
     void AddProduct(Product product);
-    void UpdateProduct(Product product);
+    void UpdateProduct(int id, Product product);
     void DeleteProduct(int id);
 
 }
