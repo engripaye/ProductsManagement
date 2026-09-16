33c5183c3d519fcfa06ea3c85a7d66c430d2fa1d
mac <@b.tunde.ipaye@gmail.com>
2026-09-16 19:52:34 +0100

    feat: dto product impl. commit by ENGR. IPAYE

---
+++ ProductsManagement/Dtos/ProductRequest.cs
@@ -0,0 +1,9 @@
+namespace ProductsManagement.Dtos;
+
+public class ProductRequest
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public required decimal Price { get; set; }
+}
