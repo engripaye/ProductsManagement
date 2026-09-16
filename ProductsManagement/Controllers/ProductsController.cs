05574d3b4b589b88c235850f4a5c82870f7af432
mac <@b.tunde.ipaye@gmail.com>
2026-09-16 19:28:14 +0100

    fix: updated product controller by ENGR,. IPAYE

--- ProductsManagement/Controllers/ProductsController.cs
+++ ProductsManagement/Controllers/ProductsController.cs
@@ -9,24 +9,18 @@ namespace ProductsManagement.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        // static List<Product> products = new List<Product>
-        // {
-        //     new Product { Id = 1, Name = "Laptop", Description = "Laptop is 12cm long", Price = 999.99M },
-        //     new Product { Id = 2, Name = "SmartPhone", Price = 499.99M },
-        //     new Product { Id = 3, Name = "iWatch", Price = 199.99M }
-        // };
-
         private readonly IProductService service;
 
         public ProductsController(IProductService productService)
         {
             service = productService;
         }
+
         // GET
         [HttpGet]
         public IActionResult GetProducts()
         {
-            
+
             return Ok(service.GetAllProducts());
         }
 
@@ -36,11 +30,12 @@ namespace ProductsManagement.Controllers
         {
             var response = service.GetProductById(id);
 
-           if (response == null)
-           {
-               return NotFound();
-           }
-           return Ok(response);
+            if (response == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(response);
         }
 
         [HttpPut]
@@ -50,30 +45,33 @@ namespace ProductsManagement.Controllers
             try
             {
                 service.UpdateProduct(id, product);
-            
+
                 return NoContent();
             }
             catch (Exception)
             {
                 return NotFound();
             }
-            
+
         }
 
         [HttpDelete]
         [Route("{id}")]
         public IActionResult DeleteProduct(int id)
         {
-            var product = products.FirstOrDefault(p => p.Id == id);
-            if (product == null)
+            try
+            {
+                service.DeleteProduct(id);
+
+                return NoContent();
+            }
+            catch (Exception)
             {
                 return NotFound();
             }
-
-            products.Remove(product);
-            
-            return NoContent();
         }
-    }
 
+    }
 }
+
+    
