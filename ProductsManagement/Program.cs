b5f0bfececff0068f4a6fffd14aac87ed5af3b4e
mac <@b.tunde.ipaye@gmail.com>
2026-09-16 19:17:14 +0100

    refractor: updated program cs updated by ENGR. IPAYE

--- ProductsManagement/Program.cs
+++ ProductsManagement/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ProductsManagement.Data;
+using ProductsManagement.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IProductService, ProductService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
