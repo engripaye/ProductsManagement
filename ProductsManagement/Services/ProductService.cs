28eaac8db949e53146d45763d31479b6d4bf0287
mac <@b.tunde.ipaye@gmail.com>
2026-09-16 19:09:48 +0100

    feat: main product service impl commit by NGR. IPAYE

---
+++ ProductsManagement/Services/ProductService.cs
@@ -0,0 +1,31 @@
+using ProductsManagement.Models;
+
+namespace ProductsManagement.Services;
+
+public class ProductService: IProductService
+{
+    public void AddProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void DeleteProduct(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product? GetProductById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateProduct(int id, Product product)
+    {
+        throw new NotImplementedException();
+    }
+}
