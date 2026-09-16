ec09c5d4289da2be52834f7e249b973f9b2bf3c9
mac <@b.tunde.ipaye@gmail.com>
2026-09-16 19:53:26 +0100

    refractor: product dto updated commit by ENGR. IPAYE

--- ProductsManagement/Dtos/ProductRequest.cs
+++ ProductsManagement/Dtos/ProductRequest.cs
@@ -2,7 +2,6 @@ namespace ProductsManagement.Dtos;
 
 public class ProductRequest
 {
-    public required int Id { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
     public required decimal Price { get; set; }
